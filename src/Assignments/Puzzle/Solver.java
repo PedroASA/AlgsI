f55f27f08812bf612d11d3304567b6b98c45f8fd
PedroASA <m.amaral.hhs@gmail.com>
2021-01-28 19:10:17 -0300

    Fix Stack Iterator

--- src/Assignments/Puzzle/Solver.java
+++ src/Assignments/Puzzle/Solver.java
@@ -2,11 +2,12 @@ import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 
-import java.util.ArrayList;
-import java.util.Stack;
+//java stack iterator is not LIFO
+import edu.princeton.cs.algs4.Stack;
 
 
 public class Solver {
+
     private class SN implements Comparable<SN>{
         Board B;
         int moves;
@@ -23,73 +24,89 @@ public class Solver {
     }
     private boolean solvable;
     private SN Sol;
+
+    private void add_nodes(SN node, MinPQ<SN> Q) {
+        for(Board x : node.B.neighbors()) {
+
+            if (node.prev == null || !x.equals(node.prev.B)) {
+                Q.insert(new SN(x, node.moves + 1, node));
+            }
+        }
+
+    }
+
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         if(initial == null) throw new IllegalArgumentException();
+
         MinPQ<SN> Q= new MinPQ<SN>();
+
+        // check if board is unsolvable. 
         MinPQ<SN> QTwin = new MinPQ<SN>();
+
         SN Node = new SN(initial, 0, null);
         Q.insert(Node);
+
+        // Check if a permutation of board is solvable.
         SN nodeTwin = new SN(initial.twin(), 0, null);
         QTwin.insert(nodeTwin);
+
         while(!Q.isEmpty()) {
+
             SN Sn = Q.delMin();
-            //System.out.println(Sn.B + "çdfçdkbkd");
+
             if(Sn.B.isGoal()) {
                 solvable = true;
                 Sol = Sn;
                 break;
             }
-            for(Board x : Sn.B.neighbors()) {
-                //System.out.println(Sn.B);
-                if (Sn.prev == null || !x.equals(Sn.prev.B)) {
-                    //System.out.println(x);
-                    Q.insert(new SN(x, Sn.moves + 1, Sn));
-                }
-            }
-            //System.out.println("dfçblmdç");
+
+            add_nodes(Sn, Q);
+
+            // if a twin board is unsolvable, try another one.
             if(QTwin.isEmpty()) {
                 SN no = new SN(Sn.B.twin(), 0, null);
                 QTwin.insert(no);
             }
+
             SN snt = QTwin.delMin();
+
+            // if any twin board is solvable, than the board received is unsolvable.
             if(snt.B.isGoal()) {
                 solvable=false;
                 break;
             }
-            //System.out.println("aaaaaa" +snt.B);
-            for(Board t : snt.B.neighbors()) {
-                //System.out.println(snt.B);
-                if (snt.prev == null || !t.equals(snt.prev.B)) {
-                    QTwin.insert(new SN(t, snt.moves + 1, snt));
-                }
-            }
+
+            add_nodes(snt, QTwin);
         }
 
     }
+
     // is the initial board solvable? (see below)
     public boolean isSolvable() {return solvable;}
 
     // min number of moves to solve initial board
     public int moves() {
-        if(solvable) return Sol.moves;
-        return -1;
+        return solvable ? Sol.moves : -1;
     }
 
     // sequence of boards in a shortest solution
     public Iterable<Board> solution() {
         if(!solvable) return null;
-        Stack<Board> P = new Stack<Board>();
+        Stack<Board> res = new Stack<Board>();
         SN s = Sol;
         while(s != null) {
-            P.add(s.B);
+            res.push(s.B);
             s=s.prev;
         }
-        return P;
+        return res;
     }
 
     // test client (see below)
     public static void main(String[] args) {
+
+        StdOut.println(args[0]);
+
         // create initial board from file
         In in = new In(args[0]);
         int n = in.readInt();
