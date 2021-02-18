5db8f35b64b14dae0f2ababe71b43e5310ad4555
PedroASA <m.amaral.hhs@gmail.com>
2021-02-18 15:47:50 -0300

    Read KdTree from File

--- src/Assignments/Kd-Trees/KdTreeVisualizer.java
+++ src/Assignments/Kd-Trees/KdTreeVisualizer.java
@@ -1,3 +1,4 @@
+
 /******************************************************************************
  *  Compilation:  javac KdTreeVisualizer.java
  *  Execution:    java KdTreeVisualizer
@@ -8,6 +9,7 @@
  *
  ******************************************************************************/
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
@@ -19,20 +21,26 @@ public class KdTreeVisualizer {
         RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
         StdDraw.enableDoubleBuffering();
         KdTree kdtree = new KdTree();
+        In in = new In(args[0]);
+        double x1 = Double.parseDouble(args[1]);
+        double y1 = Double.parseDouble(args[2]);
         while (true) {
-            if (StdDraw.isMousePressed()) {
-                double x = StdDraw.mouseX();
-                double y = StdDraw.mouseY();
-                StdOut.printf("%8.6f %8.6f\n", x, y);
-                Point2D p = new Point2D(x, y);
-                if (rect.contains(p)) {
-                    StdOut.printf("%8.6f %8.6f\n", x, y);
-                    kdtree.insert(p);
-                    StdDraw.clear();
-                    kdtree.draw();
-                    StdDraw.show();
-                }
-            }
+            try {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            StdOut.printf("%8.6f %8.6f\n", x, y);
+            Point2D p = new Point2D(x, y);
+            if (rect.contains(p)) {
+                //StdOut.printf("%8.6f %8.6f\n", x, y);
+                kdtree.insert(p);
+                StdDraw.clear();
+                kdtree.draw();
+                StdDraw.show();
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.setPenRadius(.01);
+                new Point2D(x1, y1).draw();
+            }}
+            catch(Exception e) { StdDraw.pause(10000000); }
             StdDraw.pause(20);
         }
 
